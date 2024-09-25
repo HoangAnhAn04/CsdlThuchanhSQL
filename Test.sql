54dcfb1979b54798c989a843897c8eea7b6eabb4
bob <hoanganhan04@gmail.com>
2024-09-25 20:57:57 +0700

    Refactor SQL code to comment out delete statement

--- Test.sql
+++ Test.sql
@@ -1,4 +1,4 @@
 --INSERT INTO NHANVIEN(manhanvien, ho, ten, ngaysinh, ngaylamviec, diachi, dienthoai, luongcoban, phucap)
 --VALUES (001, 'ngu', 'thien', '2004-7-18', '2024-11-1', 'haloi', 0123456789, 1000000, 0)
-DELETE FROM NHANVIEN
-WHERE manhanvien=001
+--DELETE FROM NHANVIEN
+--WHERE manhanvien=001
